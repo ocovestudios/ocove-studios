207877f935b1ba620c01a78eacfc171e2301372d
Matt Congdon <mattcongdon95@gmail.com>
2022-02-04 15:15:40 +0000

    header arrow component

---
+++ components/headerarrow.js
@@ -0,0 +1,10 @@
+const HeaderArrow = () => {
+    return (
+        <svg width="285" height="10" viewBox="0 0 285 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <line y1="-1" x2="276.27" y2="-1" transform="matrix(1 0 0 -1 0 4)" stroke="black" strokeWidth="2" />
+            <rect width="10" height="10" transform="matrix(1 0 0 -1 275 10)" fill="black" />
+        </svg>
+    )
+}
+
+export default HeaderArrow
