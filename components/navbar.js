a4637f35d8838a10099d7783fcb173af2a9693df
Matt Congdon <mattcongdon95@gmail.com>
2022-02-08 15:29:52 +0000

    nav media queries

--- components/navbar.js
+++ components/navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
             <NavbarLogo />
             <div className="nav__line"></div>
             <ul className="nav__links-list">
-                <Link href='/'><li><a className="nav__link">HOME</a></li></Link>
+                <Link href='/'><li className="nav__home"><a className="nav__link">HOME</a></li></Link>
                 <Link href='/projects'><li><a className="nav__link">PROJECTS</a></li></Link>
                 <Link href='/services'><li><a className="nav__link">SERVICES</a></li></Link>
             </ul>
