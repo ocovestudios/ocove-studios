3f160598000b8b9c385eded154891ac7a4023e1c
Arran Baker <arranbaker.design@gmail.com>
2022-02-08 14:45:36 +0000

    nav

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,11 @@
 import Link from "next/link";
-<<<<<<< HEAD
-import NavbarLogo from "../components/navbarlogo"
-=======
 import NavbarLogo from "./navbarlogo"
->>>>>>> 0e26f57b15aceaa647784569ecf4c6b24beea83c
 
 const Navbar = () => {
 
     return (
         <nav className="global-nav nav">
-<<<<<<< HEAD
-            <Link href='/'><NavbarLogo /></Link>
-=======
             <NavbarLogo />
->>>>>>> 0e26f57b15aceaa647784569ecf4c6b24beea83c
             <div className="nav__line"></div>
             <ul className="nav__links-list">
                 <Link href='/'><li><a className="nav__link">HOME</a></li></Link>
