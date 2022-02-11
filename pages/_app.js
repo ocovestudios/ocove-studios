ae5b9a4fea545be56c9256ff6e67bb9c30566f43
Matt Congdon <mattcongdon95@gmail.com>
2022-02-11 13:23:27 +0000

    ""

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/main.scss';
 import { AnimatePresence } from 'framer-motion';
+import Navbar from '../components/navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AnimatePresence exitBeforeEnter>
+      <Navbar />
       <Component {...pageProps} />
     </AnimatePresence>
   )
