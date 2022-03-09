416c1fda10c8f1560e163d7cd7c6a81bd658c253
Matt Congdon <mattcongdon95@gmail.com>
2022-03-09 10:02:30 +0000

    services: refactor markup & styles

--- components/paragrapharrow.js
+++ components/paragrapharrow.js
@@ -2,7 +2,7 @@ const ParagraphArrow = () => {
     return (
         <svg width="60" height="18" viewBox="0 0 105 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <line y1="-1.5" x2="97.9996" y2="-1.5" transform="matrix(1 2.10493e-05 -4.13903e-05 1 0.162415 6.99609)" stroke="black" strokeWidth="3" />
-            <rect x="93.0584" y="0.043335" width="11" height="11" fill="black" />
+            <rect x="93.0584" y="0.043335" width="11" height="11" fill="#292929" />
         </svg>
     )
 }
--- pages/services.js
+++ pages/services.js
@@ -13,13 +13,13 @@ const Services = () => {
 
     const servicesAssetFade = {
         initial: { opacity: 0 },
-        animate: { opacity: 1, transition: { duration: 1, delay: 1.5 } },
+        animate: { opacity: 1, transition: { duration: .6, delay: .8 } },
 
     }
 
     const servicesTextFade = {
         initial: { opacity: 0 },
-        animate: { opacity: 1, transition: { duration: 1, delay: 2 } },
+        animate: { opacity: 1, transition: { duration: .6, delay: 1.2 } },
 
     }
 
@@ -29,68 +29,72 @@ const Services = () => {
                 <title>Services | Ocove Studios</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-            <motion.div className="services--container" exit={{ opacity: 0 }}>
-                <div className="services--header-container">
-                    <motion.h2 className="services--header-title" initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.4 }}>What we do</motion.h2>
-                    <svg width="255" height="10" viewBox="0 0 285 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <motion.line y1="-1" x2="276.27" y2="-1" transform="matrix(1 0 0 -1 0 4)" stroke="black" strokeWidth="2" initial={{ x2: 0, opacity: 0 }} animate={{ x2: 276.27, opacity: 1, }} transition={{ duration: 1, ease: 'easeInOut', delay: 0.5 }} viewport={{ once: true }} />
-                        <motion.rect width="10" height="10" transform="matrix(1 0 0 -1 275 10)" fill="black" initial={{ x: 0, opacity: 0 }} animate={{ x: 276.27, opacity: 1 }} transition={{ duration: 1, ease: 'easeInOut', delay: 0.5 }} viewport={{ once: true }} />
-                    </svg>
-                </div>
-                <div className="services--content-container">
-                    <div className="services--content">
-                        <motion.div className="services--asset-container" variants={servicesAssetFade} initial='initial' animate='animate' >
-                            <div className="services--asset">
+
+            <div className="services__container">
+                <div className="services__content-container">
+
+                    <div className="services__header-container">
+                        <motion.p className="services__title--left" initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: .2 }}>What we do</motion.p>
+                        <svg width="255" height="10" viewBox="0 0 285 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <motion.line y1="-1" x2="276.27" y2="-1" transform="matrix(1 0 0 -1 0 4)" stroke="#292929" strokeWidth="2" initial={{ x2: 0, opacity: 0 }} animate={{ x2: 276.27, opacity: 1, }} transition={{ duration: .4, ease: 'easeOut', delay: .5 }} viewport={{ once: true }} />
+                            <motion.rect width="10" height="10" transform="matrix(1 0 0 -1 275 10)" fill="#292929" initial={{ x: 0, opacity: 0 }} animate={{ x: 276.27, opacity: 1 }} transition={{ duration: .4, ease: 'easeOut', delay: .5 }} viewport={{ once: true }} />
+                        </svg>
+                    </div>
+
+                    <div className="services__content--left">
+                        <div className="service__icon-container--left">
+                            <div className="service__icon">
                                 <WebIcon />
                             </div>
-                            <h3 className="services--asset-title" >WEB DEVELOPMENT</h3>
-                        </motion.div>
-                        <motion.div className='content__text' variants={servicesTextFade} initial='initial' animate='animate'>
-                            <p className="services--content-text content__text-focus">We specialise in modern web development and creative design</p>
-                            <p className="services--content-text"> We like to work with code. Whatever the project we develop an approach to meet your vision,
-                                combining cutting-edge technologies with clean aesthetics to ensure you make the best impression.  </p>
-                        </motion.div>
-                    </div >
-                    <div className="services--content">
-                        <motion.div className="services--asset-container services--asset-container-2" variants={servicesAssetFade} initial='initial' animate='animate'>
-                            <div className="services--asset services--asset-2">
-                                <ThreeDIcon />
-                            </div>
-                            <h3 className="services--asset-title asset__title-2" >3D DESIGN</h3>
-                        </motion.div>
-                        <motion.div className="content__text content__text-2 " variants={servicesTextFade} initial='initial' animate='animate' >
-                            <p className="services--content-text content__text-focus">3D modelling and asset creation</p>
-                            <p className="services--content-text"> We make custom assets in house, using the latest 2D and 3D software to
-                                produce icons, models and scenes unique to your brand, sending your online presence into the next dimension.
-                            </p>
-                        </motion.div>
+                            {/* <p className="service__icon-title" >WEB DEVELOPMENT</p> */}
+                        </div>
+                        <div className="service__text-wrap">
+                            <p className="service__text-focus">Modern web development and creative design</p>
+                            <p className="service__text"> We like to work with code. Whatever the project we develop an approach to meet your vision,
+                                combining cutting-edge technologies with clean aesthetics to ensure you make the best impression.</p>
+                        </div>
                     </div>
-                    <div className="services--content services__content-3" >
-                        <motion.div className="services--asset-container" variants={servicesAssetFade} initial='initial' animate='animate'>
-                            <div className="services--asset">
-                                {/*<LogoIcon />*/}
+
+                    <div className="services__content--right">
+                        <div className="service__icon-container--right">
+                            <div className="service__icon">
+                               <ThreeDIcon />
                             </div>
-                            <h3 className="services--asset-title">VISUAL IDENTITY</h3>
-                        </motion.div>
-                        <motion.div className="content__text" variants={servicesTextFade} initial='initial' animate='animate' >
-                            <p className="services--content-text content__text-focus">Crafting bespoke digital spaces</p>
-                            <p className="services--content-text">
-                                We work closely with people to grow their visual recognition, helping them forge a deeper connection with their audiences and
-                                customers. We carefully consider your requirements and integrate your message from the initial design to the final product.</p>
-                        </motion.div>
+                            {/* <p className="service__icon-title" >3D DESIGN</p> */}
+                        </div>
+                        <div className="service__text-wrap">
+                            <p className="service__text-focus">3D modelling and asset creation</p>
+                            <p className="service__text">We make custom assets in house, using the latest 2D and 3D software to
+                                produce bespoke icons, models and scenes unique to your brand.</p>
+                        </div>
                     </div>
-                </div >
+
+                    <div className="services__content--left">
+                        <div className="service__icon-container--left">
+                            <div className="service__icon">
+                               {/* <LogoIcon /> */}
+                            </div>
+                            {/* <p className="service__icon-title" >VISUAL IDENTITY</p> */}
+                        </div>
+                        <div className="service__text-wrap">
+                            <p className="service__text-focus">Crafting digital spaces</p>
+                            <p className="service__text">We work closely with people to grow their visual recognition, helping them forge a deeper connection with their audiences and
+                                customers. We carefully consider your requirements and integrate your message throughout the design and development process.</p>
+                        </div>
+                    </div>
+                </div>
                 <div className="services__form-container">
                     <ContactForm />
                 </div>
-                <div className="services--header-container services--contact-header" style={{ justifyContent: 'flex-end' }}>
-                    <svg width="255" height="10" viewBox="0 0 285 10" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '2rem' }}>
-                        <motion.line y1="-1" x2="276.27" y2="-1" transform="matrix(1 0 0 -1 0 4)" stroke="black" strokeWidth="2" initial={{ x2: 10, opacity: 0 }} animate={{ x2: 276.27, opacity: 1 }} transition={{ duration: 1, ease: 'easeInOut' }} viewport={{ once: true }} />
-                        <motion.rect width="10" height="10" transform="matrix(1 0 0 -1 275 10)" fill="black" initial={{ x: 10, opacity: 0 }} animate={{ x: 276.27, opacity: 1 }} transition={{ duration: 1, ease: 'easeInOut' }} viewport={{ once: true }} />
-                    </svg>
-                    <motion.h2 className="services--header-title services--contact-title" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} >Contact</motion.h2>
-                </div>
+
                 <div className="services__contact-container">
+                    <div className="services__header-container">
+                        <motion.p className="services__title--left" initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: .2 }}>Contact</motion.p>
+                        <svg width="255" height="10" viewBox="0 0 285 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <motion.line y1="-1" x2="276.27" y2="-1" transform="matrix(1 0 0 -1 0 4)" stroke="#292929" strokeWidth="2" initial={{ x2: 0, opacity: 0 }} animate={{ x2: 276.27, opacity: 1, }} transition={{ duration: .4, ease: 'easeOut', delay: .5 }} viewport={{ once: true }} />
+                            <motion.rect width="10" height="10" transform="matrix(1 0 0 -1 275 10)" fill="#292929" initial={{ x: 0, opacity: 0 }} animate={{ x: 276.27, opacity: 1 }} transition={{ duration: .4, ease: 'easeOut', delay: .5 }} viewport={{ once: true }} />
+                        </svg>
+                    </div>
                     <ul className="contact__list">
                         <li className="contact__list-item">
                             <p className="contact__name">Billy Myles-Berkouwer</p>
@@ -128,9 +132,16 @@ const Services = () => {
                     </ul>
                     <motion.div className="contact-logo-wrapper" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ ease: 'easeOut', duration: 2 }} viewport={{ once: true }}>
                         <LogoSVG />
+
                     </motion.div>
                 </div>
-            </motion.div >
+
+            </div>
+
+
+
+
+
             <Footer />
         </>
     );
