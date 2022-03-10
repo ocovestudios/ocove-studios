abf49b8376b531e2f4e9ccdb2fd3a3d58807fc01
Matt Congdon <mattcongdon95@gmail.com>
2022-03-10 15:30:57 +0000

    update privacy & cookie policy

--- pages/cookiePolicy.js
+++ pages/cookiePolicy.js
@@ -10,9 +10,14 @@ const CookiePolicy = () => {
                 <title>Cookie Policy | Ocove Studios</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-            <motion.div className="policy__page-container" initial={{ x: 1500 }} animate={{ x: 0 }} transition={{ duration: 1 }}>
-                <h2 className="policy__page-title">Cookie Policy.</h2>
-                <p className="policy__page-text">A cookie is a small file of letters and numbers that is downloaded on to your computer when you visit a website. Cookies are used by many websites and can do a number of things, eg remembering your preferences, recording what you have put in your shopping basket, and counting the number of people looking at a website.</p>
+            <motion.div className="policy__page-container" initial={{ x: 1500 }} animate={{ x: 0 }} transition={{ duration: .6, ease: "easeInOut" }}>
+                <h2 className="policy__page-title">Cookie Policy</h2>
+                <p className="policy__page-text">A cookie is a small file of letters and numbers that is downloaded onto your computer when you visit a website. They are widely used in order to make websites work, or work more efficiently, as well as to provide information to the owners of the site.</p>
+                <p className="policy__page-text">
+                This website does not use cookies to collect personally identifiable information about you as a user of the site.</p>
+                <p className="policy__page-text">
+                This website does contain hyperlinks to websites owned and operated by third parties. These websites have their own privacy policies, including cookies, and we urge you to review them. They will govern the use of any personal information you submit or which is collected by cookies while visiting these websites.
+               </p>
             </motion.div>
             <Footer />
         </>
--- pages/privacyPolicy.js
+++ pages/privacyPolicy.js
@@ -10,11 +10,11 @@ const PrivacyPolicy = () => {
                 <title>Privacy Policy | Ocove Studios</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-            <motion.div className="policy__page-container" initial={{ x: 1500 }} animate={{ x: 0 }} transition={{ duration: 1 }}>
-                <h2 className="policy__page-title">Privacy Policy.</h2>
+            <motion.div className="policy__page-container" initial={{ x: 1500 }} animate={{ x: 0 }} transition={{ duration: .6, ease: "easeInOut" }}>
+                <h2 className="policy__page-title">Privacy Policy</h2>
                 <p className="policy__page-text">In accordance with GDPR (General Data Protection Regulation) requirements, we are committed to protecting and respecting your privacy. This website does not use cookies, and personal information is not tracked or retained.
                 </p>
-                <p className="policy__page-text">Articles on this site may include embedded content (e.g. videos, images, articles, etc.). Embedded content from other websites behaves in the exact same way as if the visitor has visited the other website. These websites may collect data about you, use cookies, embed additional third-party tracking, and monitor your interaction with that embedded content, including tracking your interaction with the embedded content if you have an account and are logged in to that website.
+                <p className="policy__page-text">We may collect some personal information when you contact us, such as your name and contact details. When you voluntarily share your information with us, you consent to the processing, collecting and storing of that data.
                 </p>
             </motion.div>
             <Footer />
